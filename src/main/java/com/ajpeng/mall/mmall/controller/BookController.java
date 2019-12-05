746abe81a61b66cd28ed048aa9fe3b6675b20ae8
ajpeng <paj742257823@163.com>
2019-12-05 17:34:25 +0800

    增加日志

--- src/main/java/com/ajpeng/mall/mmall/controller/BookController.java
+++ src/main/java/com/ajpeng/mall/mmall/controller/BookController.java
@@ -2,6 +2,7 @@ package com.ajpeng.mall.mmall.controller;
 
 import com.ajpeng.mall.mmall.dao.BookDao;
 import com.ajpeng.mall.mmall.entity.Book;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
@@ -9,6 +10,7 @@ import org.springframework.web.servlet.ModelAndView;
 import javax.annotation.Resource;
 
 @Controller
+@Slf4j
 @RequestMapping("/book")
 public class BookController {
 
@@ -20,6 +22,7 @@ public class BookController {
         ModelAndView mav = new ModelAndView();
         mav.addObject("bookList", bookDao.findAll());
         mav.setViewName("book/bookList");
+        log.info("查询成功");
         return mav;
     }
 
@@ -32,6 +35,7 @@ public class BookController {
     @RequestMapping(value = "/add", method = RequestMethod.POST)
     public String add(Book book) {
         bookDao.save(book);
+        log.info("新增成功");
         return "forward:/book/list";
     }
 
@@ -40,6 +44,7 @@ public class BookController {
         ModelAndView mav = new ModelAndView();
         mav.addObject("book", bookDao.getOne(id));
         mav.setViewName("book/bookUpdate");
+        log.info("修改成功,参数:" + id);
         return mav;
     }
 
@@ -52,6 +57,7 @@ public class BookController {
     @PostMapping(value = "/update")
     public String update(Book book) {
         bookDao.save(book);
+        log.info("修改成功,参数:" + book.toString());
         return "forward:/book/list";
     }
 
@@ -64,6 +70,7 @@ public class BookController {
     @RequestMapping(value = "/delete", method = RequestMethod.GET)
     public String delete(Integer id) {
         bookDao.deleteById(id);
+        log.info("删除成功,参数:" + id);
         return "forward:/book/list";
     }
 
